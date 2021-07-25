336799738684ffddfa0f490f3eb250cdf0a157ea
bryantuwagbale <78828750+bryantuwagbale@users.noreply.github.com>
2021-07-25 15:36:57 -0500

    user.js

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model, Types } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true 
+    },
+    email: {
+        type: String,
+        required: true,
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false
+}
+);
+
+//friend count 
+userSchema.virtual('friendCount').get(function() {
+return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
