7bc35165fe7082e20e9c7f08aa0f1e9966f93b1d
bryantuwagbale <78828750+bryantuwagbale@users.noreply.github.com>
2021-07-26 00:01:20 -0500

    fixed typo in thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -55,5 +55,5 @@ thoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
 });
 
-const Thought = model('THought', thoughtSchema);
+const Thought = model('Thought', thoughtSchema);
 module.exports = Thought; 
